805020f302dbe45ae3eb1c53bad3003296123370
amol0900 <a.olsson59@gmail.com>
2020-01-17 10:29:23 +0100

    intial commit

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>hello</h2>
+            </div>
+        )
+    }
+}
+
+export default Home;
+
