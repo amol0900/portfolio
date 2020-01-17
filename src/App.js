09b638268796aec8c28deb0824c201fa2188dc83
amol0900 <a.olsson59@gmail.com>
2020-01-17 12:04:46 +0100

    home component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Curly from './curly.png';
-import Avatar from './avatar.png';
+/* import Curly from './curly.png';
+import Avatar from './avatar.png'; */
 import Main from './components/main';
 
 class App extends Component {
   render() {
     return (
-<div id="main">
-        <div className="landing">
-          <img src={Avatar} alt="portrait" className="avatar" />
-        <img src={Curly} alt="bracket" className="bracket" />
-
-  <div className="quote"><h1>Hello there.</h1>
-  <p>My name is Amanda.</p>
-  <p>I'm a Front-end Developer.</p>
-          </div>
-
-        </div>
-  </div>
+<div><Main /></div>
     );
   }
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Curly from './images/curly.png';
+import Avatar from './images/avatar.png';
 
 export class Home extends Component {
     render() {
         return (
-            <div>
-                <h2>hello</h2>
+            <div id="main">
+                <div className="landing">
+                    <img src={Avatar} alt="portrait" className="avatar" />
+                    <img src={Curly} alt="bracket" className="bracket" />
+
+                    <div className="quote"><h1>Hello there.</h1>
+                        <p>My name is Amanda.</p>
+                        <p>I'm a Front-end Developer.</p>
+                    </div>
+
+                </div>
             </div>
         )
     }
