6b4d852e899bb53137cfa61edf1ff1b9588bd2d6
amol0900 <a.olsson59@gmail.com>
2020-06-04 14:53:32 +0200

    final-ver

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,6 +8,7 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className="page-container page">
+                <noscript>You need to enable JavaScript to run this app.</noscript>
                 {/* <img src={Fig} alt="fig" className="fig" /> */}
                 <div className="headwrap" style={{ display: 'flex', justifyContent: 'flex-end' }}>
                     <header>
@@ -24,8 +25,8 @@ export default class Home extends React.Component {
                         {/* <img src={Curly} alt="bracket" className="bracket" /> */}
 
                         <div className="quote"><h1>Hello there.</h1>
-                            <p class="name">My name is <span className="bottom">Amanda.</span></p>
-                            <p class="title">I'm a Front-end Developer.</p>
+                            <p className="name">My name is <span className="bottom">Amanda.</span></p>
+                            <p className="title">I'm a Front-end Developer.</p>
                         </div>
                     </div>
                 </div>
